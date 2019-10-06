e6d07b0ead236ef00ab4e4c742d23f8456c91e51
bhirschberger98 <bretthirschberger@gmail.com>
2019-10-06 18:10:11 -0400

    Initial commit, Copied code from project 1

---
+++ app/src/main/java/com/bretthirschberger/project2/AccountHome.java
@@ -0,0 +1,48 @@
+package com.bretthirschberger.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class AccountHome extends AppCompatActivity {
+
+    private TextView mNameView;
+    private TextView mDOBView;
+    private TextView mEmailView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_home);
+        mNameView = findViewById(R.id.name_view);
+        mDOBView = findViewById(R.id.dob_view);
+        mEmailView = findViewById(R.id.email_view);
+        displayAccountInfo();
+    }
+
+
+
+    public void displayAccountInfo(){
+        String email= getIntent().getStringExtra("email");
+        try (Scanner reader = new Scanner(new File(getFilesDir().getAbsolutePath() + "/users.txt"))) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] userInfo = line.split(",");
+                Log.i("File Line", line);
+                if (userInfo[0].equals(email)) {
+                    mEmailView.setText(userInfo[0]);
+                    mNameView.setText(userInfo[1]);
+                    mDOBView.setText(userInfo[2]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/bretthirschberger/project2/MainActivity.java
@@ -0,0 +1,84 @@
+package com.bretthirschberger.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+//    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, SPLASH_TIME_OUT);
+
+        mEmailField = findViewById(R.id.email_field_1);
+        mPasswordField = findViewById(R.id.password_field_1);
+
+        //fills in edit text fields on rotation
+        if (savedInstanceState != null) {
+            mEmailField.setText(savedInstanceState.getString("email","ERROR"));
+            mPasswordField.setText(savedInstanceState.getString("password","ERROR"));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("email", mEmailField.getText().toString());
+        outState.putString("password", mPasswordField.getText().toString());
+    }
+
+    public void login(View view) {
+        String email = mEmailField.getText().toString().trim();
+        if (checkValidLogin()) {
+            startActivity(new Intent(this, AccountHome.class).putExtra("email", email));
+        } else {
+            Toast.makeText(getApplicationContext(), getResources().getString(R.string.login_failed), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean checkValidLogin() {
+        String email = mEmailField.getText().toString().trim();
+        String password = mPasswordField.getText().toString().trim();
+        try (Scanner reader = new Scanner(new File(getFilesDir().getAbsolutePath() + "/users.txt"))) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] userInfo = line.split(",");
+                if (userInfo[0].equals(email) && userInfo[3].trim().equals(password)) {
+                    return true;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    public void goToRegister(View view) {
+        startActivity(new Intent(this, RegistrationActivity.class));
+    }
+}
